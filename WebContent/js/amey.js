a7c20b24838d925c6109eb0ba48ce321c3b61c9d
Amey Jain <a.ameyjain@hotmail.com>
2015-05-23 20:18:23 -0400

    section 5 fill with javascript

--- WebContent/js/amey.js
+++ WebContent/js/amey.js
@@ -11,3 +11,42 @@ function addInput(divName){
           counter++;
      }
 }
+
+var mytextbox = document.getElementById('textarea1');
+var mydropdown = document.getElementById('selectbasic1');
+
+mydropdown.onchange = function(){
+	if(this.value==1){
+     mytextbox.innerHTML  = "name of financial institution has no affiliates";
+   }
+   if(this.value==2){
+     mytextbox.innerHTML  = "[name of financial institution] does not share with our affiliates";
+   }
+if(this.value==3){
+     mytextbox.innerHTML  = "Our affiliates include companies with a [common corporate identity of financial institution] name; financial companies such as [insert illustrative list of companies]; nonfinancial companies, such as [insert illustrative list of companies;] and others, such as [insert illustrative list].";
+   }
+}
+var mytextbox2 = document.getElementById('textarea2');
+var mydropdown2 = document.getElementById('selectbasic2');
+
+mydropdown2.onchange = function(){
+	if(this.value==1){
+     mytextbox2.innerHTML  = "[name of financial institution] does not share with nonaffiliates so they can market to you";
+   }
+   if(this.value==2){
+     mytextbox2.innerHTML  = "‘Nonaffiliates we share with can include [list categories of companies such as mortgage companies, insurance companies, direct marketing companies, and nonprofit organizations].";
+   }
+	
+}
+var mytextbox3 = document.getElementById('textarea3');
+var mydropdown3 = document.getElementById('selectbasic3');
+
+mydropdown3.onchange = function(){
+	if(this.value==1){
+     mytextbox3.innerHTML  = "“[name of financial institution] doesn’t jointly market";
+   }
+   if(this.value==2){
+     mytextbox3.innerHTML  = " [list categories of companies such as credit card companies]"
+   }
+	
+}
