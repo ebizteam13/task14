98f6fd626f155eb9f98a6725623e00fbc3c09358
skandy <skandan.skandy@gmail.com>
2015-05-23 20:24:40 -0400

    javascript validation for word count for 4.3(max of 30 words) and validation for 4.2 (max of 5 options)

---
+++ WebContent/js/check.js
@@ -0,0 +1,23 @@
+﻿function check(){
+
+var options = document.getElementById('selectmultiple').options, count = 0;
+for (var i=0; i < options.length; i++) {
+	if(count>5){
+		options[i].selected = false;
+	}
+	else if (options[i].selected) 
+	 	count++;
+}
+if(count>5){
+	alert("you should select only 5 options");
+		
+}
+}
+
+function check_wordlength(){
+var str = document.getElementById('personal');
+var options=str.value.split(" ");
+if(options.length>30){
+	alert("only allowed to enter 30 words ");
+	}
+}
