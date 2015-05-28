86103f948c5cd20c240b2ae297ed2a1f71bd03b4
Amey Jain <a.ameyjain@hotmail.com>
2015-05-28 17:16:48 -0400

    section 5 uses name of institution from q1_1

--- WebContent/js/amey.js
+++ WebContent/js/amey.js
@@ -12,16 +12,21 @@ function addInput(divName){
           counter++;
      }
 }
+name;
+function setName(){
+  name=document.getElementById("q1_1").value;
+  window.alert(name);
+}
 
 var mytextbox = document.getElementById('textarea1');
 var mydropdown = document.getElementById('selectbasic1');
 
 mydropdown.onchange = function(){
 	if(this.value==1){
-     mytextbox.innerHTML  = "[name of financial institution] has no affiliates";
+     mytextbox.innerHTML  = name+" has no affiliates";
    }
    if(this.value==2){
-     mytextbox.innerHTML  = "[name of financial institution] does not share with our affiliates";
+     mytextbox.innerHTML  = name+" does not share with our affiliates";
    }
 if(this.value==3){
      mytextbox.innerHTML  = "Our affiliates include companies with a [common corporate identity of financial institution] name; financial companies such as [insert illustrative list of companies]; nonfinancial companies, such as [insert illustrative list of companies;] and others, such as [insert illustrative list].";
@@ -32,7 +37,7 @@ var mydropdown2 = document.getElementById('selectbasic2');
 
 mydropdown2.onchange = function(){
 	if(this.value==1){
-     mytextbox2.innerHTML  = "[name of financial institution] does not share with nonaffiliates so they can market to you";
+     mytextbox2.innerHTML  = name+" does not share with nonaffiliates so they can market to you";
    }
    if(this.value==2){
      mytextbox2.innerHTML  = "Nonaffiliates we share with can include [list categories of companies such as mortgage companies, insurance companies, direct marketing companies, and nonprofit organizations].";
@@ -44,7 +49,7 @@ var mydropdown3 = document.getElementById('selectbasic3');
 
 mydropdown3.onchange = function(){
 	if(this.value==1){
-     mytextbox3.innerHTML  = "[name of financial institution] doesn’t jointly market";
+     mytextbox3.innerHTML  = name+" doesn’t jointly market";
    }
    if(this.value==2){
      mytextbox3.innerHTML  = " [list categories of companies such as credit card companies]"
